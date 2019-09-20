649c36cce6176f54d41d42dd84acdc2f720fd98e
Jacob Otten <jtotten@ksu.edu>
2019-09-19 19:22:49 -0500

    commit changes to add kelvin conversion

--- temperature.sh
+++ temperature.sh
@@ -4,5 +4,6 @@ echo "Enter temperature in F:"
 read tempf
 echo "Temperature in degrees C:"
 echo "scale=3;(($tempf-32.0)*(5.0/9.0))" | bc
-
+echo "Temp in degrees Kelvin: "
+echo "scale=3; (($tempf-32)*(5/9)+273)" | bc
 
